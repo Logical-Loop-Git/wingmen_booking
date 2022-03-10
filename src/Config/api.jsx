85c3b19d3711aff8edd238a09750455c024f3267
Kishan Gaywala <kishan.gaywala@gmail.com>
2022-03-10 16:31:25 +0530

    domain

--- src/Config/api.jsx
+++ src/Config/api.jsx
@@ -2,10 +2,10 @@
 // const API = "http://192.168.29.90:3000/api/v1/user/"
 
 // AWS SERVER API
-const API = "http://18.219.159.1:3000/api/v1/user/"
+const API = "https://app.mywngmn.com/api/v1/user/"
 
 //GLOBAL IMAGE URL KEY
-export const imageUrl = "http://18.219.159.1:3000/";
+export const imageUrl = "https://app.mywngmn.com/";
 
 //STRIP TESTING KEY
 export const stripTestKey = "pk_test_xICCC7UvMO1QVnvHYI4pjmcM002SNKwIhE";
