119e7c7a4c9ac68dd212af21a0a8a814e439eb4f
Kishan Gaywala <kishan.gaywala@gmail.com>
2022-04-29 13:55:24 +0530

    event Details changes

--- src/Pages/EventDetails.jsx
+++ src/Pages/EventDetails.jsx
@@ -15,7 +15,7 @@ const EventDetails = () => {
     } = useContext(Context)
     const [eventData, setEventData] = useState({})
     const [cardIdData, setCardIdData] = useState({})
-    const cancelTime = moment(moment(eventData.bookingDate).format('YYYY-MM-DD hh:mm:ss')).subtract(0, "hour").format('YYYY-MM-DD hh:mm:ss')
+    const cancelTime = moment(moment(eventData.bookingDate).format('YYYY-MM-DD hh:mm:ss')).subtract(5, "hour").format('YYYY-MM-DD hh:mm:ss')
     const currentDate = moment().format('YYYY-MM-DD hh:mm:ss')
 
     const fetchBookingData = () => {
@@ -88,56 +88,61 @@ const EventDetails = () => {
                             <h3>Event Booking Information</h3>
                         </div>
                         {
-                            eventData.isDeleted === true ? (<div className="schedule_booking_footer">
-                                <div className="user_info">
-                                    <h4 style={{ marginLeft: 20 }}>Your Booking has been cancelled</h4>
-                                </div>
-                            </div>) :
-                                (<div className="schedule_booking_footer">
+                            eventData._id ?
+                                ((eventData.isDeleted === true && eventData.isDeleted) ? (<div className="schedule_booking_footer">
                                     <div className="user_info">
-                                        <h5 style={{ marginBottom: 20 }}>User Details</h5>
-                                        <h6>Name: <span>{eventData.firstName} {eventData.lastName}</span></h6>
-                                        <h6>Phone Number: <span>+{eventData.countryCode} {eventData.phone}</span></h6>
-                                        {
-                                            eventData.altPhone === null ? null :
-                                                <h6>Alternative Phone Number: <span>+{eventData.altCountryCode} {eventData.altPhone}</span></h6>
+                                        <h4 style={{ marginLeft: 20 }}>Your Booking has been cancelled</h4>
+                                    </div>
+                                </div>) :
+                                    (<div className="schedule_booking_footer">
+                                        <div className="user_info">
+                                            <h5 style={{ marginBottom: 20 }}>User Details</h5>
+                                            <h6>Name: <span>{eventData.firstName} {eventData.lastName}</span></h6>
+                                            <h6>Phone Number: <span>+{eventData.countryCode} {eventData.phone}</span></h6>
+                                            {
+                                                eventData.altPhone === null ? null :
+                                                    <h6>Alternative Phone Number: <span>+{eventData.altCountryCode} {eventData.altPhone}</span></h6>
 
-                                        }
-                                    </div>
-                                    <div className="schedule_info">
-                                        <h5 style={{ marginBottom: 20 }}>Booking Details</h5>
-                                        <h6>Event Address: <span>{eventData.pickUpAddress}</span></h6>
-                                        <h6>Drivers: <span>{eventData.driver}</span></h6>
-                                        <h6>Co-drivers: <span>{eventData.coDriver}</span></h6>
-                                        <h6>Booking No.: <span>{eventData.bookingNo}</span></h6>
-                                        <h6>Booking Date: <span>{eventData.bookingDate ? moment.utc(eventData.bookingDate).format('MMMM DD YYYY') : ""}</span></h6>
-                                        <h6>Booking Time: <span>{eventData.bookingDate ? moment.utc(eventData.bookingDate).format('hh:mm:ss a') : ""}</span></h6>
-                                    </div>
-                                    <div className="schedule_info">
-                                        <h5 style={{ margin: "20px 0px 20px 0px" }}>Card Details</h5>
-                                        <div className="display_user_card">
-                                            <label>
-                                                <p>•••• •••• •••• {cardIdData.last4Digits}</p>
-                                                <p>{cardIdData.brand} - {cardIdData.expiryDate}</p>
-                                            </label>
+                                            }
                                         </div>
-                                    </div>
-                                    {
-                                        (cancelTime < currentDate) === true ? null :
-
-                                            <div style={{ marginTop: 20 }} className='d-flex justify-content-between'>
-                                                {
-                                                    isLoading === true ? <button className="booking_next">
-                                                        <div class="spinner-border text-white" role="status">
-                                                            <span class="visually-hidden">Loading...</span>
-                                                        </div>
-                                                    </button> : <button className="booking_next" style={{ marginBottom: 20 }} onClick={cancelBooking}>
-                                                        Cancel Booking
-                                                    </button>
-                                                }
+                                        <div className="schedule_info">
+                                            <h5 style={{ marginBottom: 20 }}>Booking Details</h5>
+                                            <h6>Event Address: <span>{eventData.pickUpAddress}</span></h6>
+                                            <h6>Drivers: <span>{eventData.driver}</span></h6>
+                                            <h6>Co-drivers: <span>{eventData.coDriver}</span></h6>
+                                            <h6>Booking No.: <span>{eventData.bookingNo}</span></h6>
+                                            <h6>Booking Date: <span>{eventData.bookingDate ? moment.utc(eventData.bookingDate).format('MMMM DD YYYY') : ""}</span></h6>
+                                            <h6>Booking Time: <span>{eventData.bookingDate ? moment.utc(eventData.bookingDate).format('hh:mm:ss a') : ""}</span></h6>
+                                        </div>
+                                        <div className="schedule_info">
+                                            <h5 style={{ margin: "20px 0px 20px 0px" }}>Card Details</h5>
+                                            <div className="display_user_card">
+                                                <label>
+                                                    <p>•••• •••• •••• {cardIdData.last4Digits}</p>
+                                                    <p>{cardIdData.brand} - {cardIdData.expiryDate}</p>
+                                                </label>
                                             </div>
-                                    }
-                                </div>)
+                                        </div>
+                                        {
+                                            (cancelTime < currentDate) === true ? null :
+
+                                                <div style={{ marginTop: 20 }} className='d-flex justify-content-between'>
+                                                    {
+                                                        isLoading === true ? <button className="booking_next">
+                                                            <div class="spinner-border text-white" role="status">
+                                                                <span class="visually-hidden">Loading...</span>
+                                                            </div>
+                                                        </button> : <button className="booking_next" style={{ marginBottom: 20 }} onClick={cancelBooking}>
+                                                            Cancel Booking
+                                                        </button>
+                                                    }
+                                                </div>
+                                        }
+                                    </div>)) : <div className="d-flex justify-content-center m-3">
+                                    <div className="spinner-border" style={{ color: "#05a0e0" }} role="status">
+                                        <span className="visually-hidden"></span>
+                                    </div>
+                                </div>
                         }
                     </Card>
                 </div>
